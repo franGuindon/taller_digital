48774094b3e1de30786d0c9b9f7864fac7fc3a1a
franGuindon <fbadilla10@gmail.com>
2021-08-17 12:16:44 -0600

    mux: Modifica mux para ser combinacional

--- Lab_2/mux_4_1.v
+++ Lab_2/mux_4_1.v
@@ -2,15 +2,13 @@
 
 module mux_4_1
   #(DATA_WIDTH=4)(input [DATA_WIDTH-1:0]  in0, in1, in2, in3,
-		  input [1:0] 		      sel,
-		  output reg [DATA_WIDTH-1:0] out);
-
-   always @ (sel or in0 or in1 or in2 or in3)
-     case (sel)
-       2'b00: out = in0;
-       2'b01: out = in1;
-       2'b10: out = in2;
-       2'b11: out = in3;
-     endcase // case (sel)
+		  input [1:0] 		  sel,
+		  output [DATA_WIDTH-1:0] out);
+   case (sel)
+     2'b00: assign out = in0;
+     2'b01: assign out = in1;
+     2'b10: assign out = in2;
+     2'b11: assign out = in3;
+   endcase // case (sel)
 endmodule // mux_4_1
 
